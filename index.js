a055dc606f836508f92c194b12d03e481acbff23
jonathan <jonathan.123kutumbakana@gmail.com>
2023-12-13 17:27:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) { return blocks - 42; }
+function distanceFromHqInFeet(feet){
+  distanceFromHqInBlocks(blocks);
+  return feet * 264;
+}
+function distanceTravelledInFeet(start, destination){return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);}
+function calculatesFarePrice(start, destination){return distanceTravelledInFeet(start, destination) * 264;}
